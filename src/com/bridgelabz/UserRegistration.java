4160aa74ab8516911bc4ea28979b1d7c0a513a4b
hibo2261 <hborse1@gmail.com>
2022-09-28 01:22:03 +0530

    [Himanshu] : Added Ability To Validate The Password With Rule2

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -21,7 +21,7 @@ public class UserRegistration {
          */
 
         boolean isPassword;
-        String mobileNumberRegex = "^[A-z a-z]{8}$";;
+        String mobileNumberRegex = "^[A-Z]{1}[a-z]{7,}$";;
         Pattern obj = Pattern.compile(mobileNumberRegex);
         Matcher matcherObj = obj.matcher(password);
         isPassword =  matcherObj.matches();
